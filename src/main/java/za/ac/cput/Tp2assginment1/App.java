e1faeba6a50d6e48cfe2af90880871c5660228df
AbelNgal <abelngalimea01@gmail.com>
2016-03-05 18:32:09 +0200

    First commit

---
+++ src/main/java/za/ac/cput/Tp2assginment1/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.Tp2assginment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
